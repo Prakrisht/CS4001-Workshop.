52b09f17e44267fbc77db1301de3e572e7945937
Prakrisht Shrestha <prakrisht03@gmail.com>
2025-11-25 16:44:17 +0545

    Add files via upload

---
+++ LiteralPractice.java
@@ -0,0 +1,20 @@
+package week3workshop;
+import java.util.Scanner;
+public class LiteralPractice
+{
+   public static void main()
+   {
+       long a=1203L;
+       float b=3.22f;
+       String copyright="\u00A9";
+       
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println("Copyright symbol:"+copyright);
+       
+       
+       
+
+       
+   }
+}
